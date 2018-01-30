d17efb80a99ec66735949976b443ec53ff0e1f6c
lxg <lxg1986@gmail.com>
2018-01-30 23:34:37 +0800

    http to https

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,32 +16,40 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    let url = 'http://wxif.boheyayi.com/doctor/doctorlist';
+    console.log(url,'this is url');
+    wx.request({
+      url,
+      success: function(res) {
+        console.log(res.data)
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    })
+    //if (app.globalData.userInfo) {
+    //  this.setData({
+    //    userInfo: app.globalData.userInfo,
+    //    hasUserInfo: true
+    //  })
+    //} else if (this.data.canIUse){
+    //  // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //  // 所以此处加入 callback 以防止这种情况
+    //  app.userInfoReadyCallback = res => {
+    //    this.setData({
+    //      userInfo: res.userInfo,
+    //      hasUserInfo: true
+    //    })
+    //  }
+    //} else {
+    //  // 在没有 open-type=getUserInfo 版本的兼容处理
+    //  wx.getUserInfo({
+    //    success: res => {
+    //      app.globalData.userInfo = res.userInfo
+    //      this.setData({
+    //        userInfo: res.userInfo,
+    //        hasUserInfo: true
+    //      })
+    //    }
+    //  })
+    //}
   },
   getUserInfo: function(e) {
     console.log(e)
