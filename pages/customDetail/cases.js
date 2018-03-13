78e97768af9092988f81812caa02bbf5aaed2c94
lxg <lxg1986@gmail.com>
2018-03-13 22:10:24 +0800

    change image position

--- pages/customDetail/cases.js
+++ pages/customDetail/cases.js
@@ -5,10 +5,10 @@ export default [
     "avatar" : "../../assets/case1.jpeg",
     "history_num": 59417707,
     "description": "病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙",
-    "before":"../../assets/before.jpeg",
-    "after" : "../../assets/after.jpeg",
+    "before":"../../assets/songguangying/before.jpeg",
+    "after": "../../assets/songguangying/after.jpeg",
     "date": "2013.08～2015.09",
-    "plan": "减数14，24，34，44;Pass矫治技术（XBT颊管＋MLF托槽）;上下颌强支抗",
+    "plan": "减数14，24，34，44；Pass矫治技术（XBT颊管＋MLF托槽）；上下颌强支抗",
     "hard" : "支抗控制 前牙转矩控制"
   }
 ]
