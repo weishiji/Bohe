40867ea008ac7d239eaf787be6acf6f0a84c1e36
lxg <lxg1986@gmail.com>
2018-03-21 11:15:43 +0800

    增加jiangruoping图片

--- pages/customDetail/cases.js
+++ pages/customDetail/cases.js
@@ -7,14 +7,70 @@ export default [
         "avatar": "../../assets/case1.jpeg",
         "history_num": 59417707,
         "description": "病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙。",
-        "before": "../../assets/songguangying/before.jpeg",
-        "after": "../../assets/songguangying/after.jpeg",
+        "diff" : [
+          {
+            "before": "../../assets/songguangying/before.jpeg",
+            "after": "../../assets/songguangying/after.jpeg",
+          }
+        ],
         "date": "2013.08～2015.09",
         "plan": "减数14，24，34，44；Pass矫治技术（XBT颊管＋MLF托槽）；上下颌强支抗",
         "hard": "支抗控制 前牙转矩控制",
         "star": '4_5'
       }
     ],
-    
+  },
+  {
+    "id": 56,
+    "cases": [
+      {
+        "name": "xx女士",
+        "avatar": "../../assets/jiangruoping/avatar1.jpg",
+        "history_num": 23438,
+        "description": "病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙。",
+        "diff": [
+          {
+            "before": "../../assets/jiangruoping/1before1.jpg",
+            "after": "../../assets/jiangruoping/1after1.jpg",
+          },
+          {
+            "before": "../../assets/jiangruoping/1before2.jpg",
+            "after": "../../assets/jiangruoping/1after2.jpg",
+          },
+          {
+            "before": "../../assets/jiangruoping/1before3.jpg",
+            "after": "../../assets/jiangruoping/1after3.jpg",
+          }
+        ],
+        "date": "2013.08～2015.09",
+        "plan": "减数14，24，34，44；Pass矫治技术（XBT颊管＋MLF托槽）；上下颌强支抗",
+        "hard": "支抗控制 前牙转矩控制",
+        "star": '4_5'
+      },
+      {
+        "name": "xx女士",
+        "avatar": "../../assets/jiangruoping/avatar2.jpg",
+        "history_num": 23438,
+        "description": "病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙病例特点 突面型，嘴突，俗称龅牙。",
+        "diff": [
+          {
+            "before": "../../assets/jiangruoping/2before1.jpg",
+            "after": "../../assets/jiangruoping/2after1.jpg",
+          },
+          {
+            "before": "../../assets/jiangruoping/2before2.jpg",
+            "after": "../../assets/jiangruoping/2after2.jpg",
+          },
+          {
+            "before": "../../assets/jiangruoping/2before3.jpg",
+            "after": "../../assets/jiangruoping/2after3.jpg",
+          }
+        ],
+        "date": "2013.08～2015.09",
+        "plan": "减数14，24，34，44；Pass矫治技术（XBT颊管＋MLF托槽）；上下颌强支抗",
+        "hard": "支抗控制 前牙转矩控制",
+        "star": '4_5'
+      }
+    ],
   }
 ]
